00332b65585941e1cb6c047d744ff8647c035c2f
Bruno Cruz <bru.cruz@gmail.com>
2024-11-07 10:04:26 -0300

    add confession schedule tests

--- app/models/confession_schedule.rb
+++ app/models/confession_schedule.rb
@@ -1,3 +1,6 @@
 class ConfessionSchedule < ApplicationRecord
-  belongs_to :church
+  belongs_to :church, optional: false
+
+  validates :day_of_week, presence: true, inclusion: { in: 0..6 }
+  validates :start_time, :active, presence: true
 end
--- test/models/confession_schedule_test.rb
+++ test/models/confession_schedule_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 class ConfessionScheduleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @church = churches(:santo_antonio_de_piracicaba)
+    @confession_schedule = confession_schedules(:one)
+  end
+
+  test "should be valid with all required attributes" do
+    assert @confession_schedule.valid?
+  end
+
+  test "should belong to church" do
+    assert_equal @church, @confession_schedule.church
+  end
+
+  test "should require church" do
+    @confession_schedule.church = nil
+    assert_not @confession_schedule.valid?
+  end
+
+  test "should require day_of_week" do
+    @confession_schedule.day_of_week = nil
+    assert_not @confession_schedule.valid?
+  end
+
+  test "should validate day_of_week is between 0 and 6" do
+    @confession_schedule.day_of_week = 7
+    assert_not @confession_schedule.valid?
+
+    @confession_schedule.day_of_week = -1
+    assert_not @confession_schedule.valid?
+  end
+
+  test "should require start_time" do
+    @confession_schedule.start_time = nil
+    assert_not @confession_schedule.valid?
+  end
+
+  test "should require active" do
+    @confession_schedule.active = nil
+    assert_not @confession_schedule.valid?
+  end
+
+  test "should allow end_time to be nil" do
+    @confession_schedule.end_time = nil
+    assert @confession_schedule.valid?
+  end
 end
