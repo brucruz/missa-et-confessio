7f2c656346da25d8965b8f54d0d3377922ac1a3d
Bruno Cruz <bru.cruz@gmail.com>
2024-11-07 10:02:52 -0300

    add mass schedule tests

--- app/models/mass_schedule.rb
+++ app/models/mass_schedule.rb
@@ -1,3 +1,6 @@
 class MassSchedule < ApplicationRecord
-  belongs_to :church
+  belongs_to :church, optional: false
+
+  validates :day_of_week, presence: true, inclusion: { in: 0..6 }
+  validates :start_time, :active, presence: true
 end
--- test/models/mass_schedule_test.rb
+++ test/models/mass_schedule_test.rb
@@ -1,7 +1,44 @@
 require "test_helper"
 
 class MassScheduleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @church = churches(:santo_antonio_de_piracicaba)
+    @mass_schedule = mass_schedules(:one)
+  end
+
+  test "should be valid with all required attributes" do
+    assert @mass_schedule.valid?
+  end
+
+  test "should belong to church" do
+    assert_equal @church, @mass_schedule.church
+  end
+
+  test "should require church" do
+    @mass_schedule.church = nil
+    assert_not @mass_schedule.valid?
+  end
+
+  test "should require day_of_week" do
+    @mass_schedule.day_of_week = nil
+    assert_not @mass_schedule.valid?
+  end
+
+  test "should validate day_of_week is between 0 and 6" do
+    @mass_schedule.day_of_week = 7
+    assert_not @mass_schedule.valid?
+
+    @mass_schedule.day_of_week = -1
+    assert_not @mass_schedule.valid?
+  end
+
+  test "should require start_time" do
+    @mass_schedule.start_time = nil
+    assert_not @mass_schedule.valid?
+  end
+
+  test "should require active" do
+    @mass_schedule.active = nil
+    assert_not @mass_schedule.valid?
+  end
 end
